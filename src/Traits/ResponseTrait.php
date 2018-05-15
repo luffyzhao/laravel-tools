ee0004761bdf993c960723c4a190187a282ff535
luffyzhao <luffyzhao@vip.126.com>
2018-05-15 09:55:27 +0800

    添加ResponseTrait类

---
+++ src/Traits/ResponseTrait.php
@@ -0,0 +1,68 @@
+<?php
+namespace luffyzhao\laravelTools\Traits;
+
+
+trait ResponseTrait
+{
+    /**
+     * 登录响应.
+     *
+     * @method respondWithToken
+     *
+     * @param [type] $token [description]
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @author luffyzhao@vip.126.com
+     */
+    protected function respondWithToken($token)
+    {
+        return $this->respondWithSuccess([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ]);
+    }
+
+    /**
+     * 错误响应.
+     *
+     * @method respondWithError
+     *
+     * @param [type] $message [description]
+     * @param int    $code    [description]
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @author luffyzhao@vip.126.com
+     */
+    protected function respondWithError($message, $code = 500)
+    {
+        return response()->json([
+            'data' => [],
+            'code' => $code,
+            'message' => $message,
+        ], $code);
+    }
+
+    /**
+     * 成功响应.
+     *
+     * @method respondWithSuccess
+     *
+     * @param [type] $data    [description]
+     * @param string $message [description]
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @author luffyzhao@vip.126.com
+     */
+    protected function respondWithSuccess($data, $message = '成功')
+    {
+        return response()->json([
+            'data' => $data,
+            'code' => 200,
+            'message' => $message,
+        ]);
+    }
+}
