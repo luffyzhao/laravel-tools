b7dbe0a0290118d677703f5cde4f9ea6ef373924
luffyzhao <luffyzhao@vip.126.com>
2018-05-29 15:03:03 +0800

    把签名放header里

--- src/Sign/Drivers/Md5Sign.php
+++ src/Sign/Drivers/Md5Sign.php
@@ -3,51 +3,61 @@
 namespace luffyzhao\laravelTools\Sign\Drivers;
 
 use luffyzhao\laravelTools\Sign\CoreSign;
+use Illuminate\Support\Facades\Config;
 
 class Md5Sign extends CoreSign
 {
     /**
-     * 签名
+     * 签名.
+     *
      * @method sign
+     *
      * @param array $data
      *
      * @return string
      *
      * @author luffyzhao@vip.126.com
      */
-    public function sign(array $data) : string {
+    public function sign(array $data): string
+    {
         // 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
         $prestr = $this->createLinkstring($this->sortKeys($data));
 
-        return md5($prestr . $this->getSignKey());
+        return md5($prestr.$this->getSignKey());
     }
 
     /**
-     * 验证签名
+     * 验证签名.
+     *
      * @method verify
-     * @param array $data
+     *
+     * @param array  $data
      * @param string $sign
      *
      * @return bool
      *
      * @author luffyzhao@vip.126.com
      */
-    public function verify(array $data, string $sign) : bool{
+    public function verify(array $data, string $sign): bool
+    {
         $prestr = $this->createLinkstring($this->sortKeys($data));
 
-        $prestrSign = md5($prestr . $this->getSignKey());
+        $prestrSign = md5($prestr.$this->getSignKey());
+
         return $prestrSign == $sign;
     }
 
     /**
-     * 获取加密串
+     * 获取加密串.
+     *
      * @method getSignKey
      *
      * @return string
      *
      * @author luffyzhao@vip.126.com
      */
-    private function getSignKey() : string {
+    private function getSignKey(): string
+    {
         return Config::get('app.sign_key', 'Q1/bJIBw35vq3RUmXfespleZkfIiA2nKD8JXIdCwiBY=');
 //        return 'Q1/bJIBw35vq3RUmXfespleZkfIiA2nKD8JXIdCwiBY=';
     }
--- src/Sign/SignManager.php
+++ src/Sign/SignManager.php
@@ -9,85 +9,113 @@ use luffyzhao\laravelTools\Sign\Exceptions\SignException;
 
 class SignManager
 {
-
     /**
-     * 加签
+     * 加签.
+     *
      * @method sign
-     * @param array $request
+     *
+     * @param array  $request
      * @param string $signType
      *
      * @return array
+     *
      * @throws SignException
      *
      * @author luffyzhao@vip.126.com
      */
-    public function sign(array $request, $signType = 'md5') : array{
-        $data = collect($request)->except(['_sign', '_sign_type'])->put('_timestamp', Carbon::now()->format('Y-m-d H:i:s'))->all();
-        $data['_sign'] = $this->signObj($signType)->sign($data);
-        $data['_sign_type'] = $signType;
-        return $data;
+    public function sign(array $request, $signType = 'md5'): array
+    {
+        $timestamp = Carbon::now()->format('Y-m-d H:i:s');
+        $data = collect($request)->except(['_sign', '_sign_type'])->put('_timestamp', $timestamp)->all();
+
+        return [
+          '_sign' => $this->signObj($signType)->sign($data),
+          '_sign_type' => $signType,
+          '_timestamp' => $timestamp,
+        ];
     }
+
     /**
      * 验证
+     *
      * @method validate
+     *
      * @param array $request
      *
      * @return bool
+     *
      * @throws SignException
      *
      * @author luffyzhao@vip.126.com
      */
-    public function validate(array $request) : bool
+    public function validate(array $request): bool
     {
         $data = collect($request)->except(['_sign', '_sign_type'])->all();
         list($sign, $signType) = $this->validateParams($request);
+
         return $this->signObj($signType)->verify($data, $sign);
     }
 
     /**
-     * 验证参数
+     * 验证参数.
+     *
      * @method validateParams
+     *
      * @param array $request
      *
      * @return array
      *
      * @author luffyzhao@vip.126.com
      */
-    protected function validateParams(array $request) : array {
-        return collect($request)->only('_sign', '_sign_type', '_timestamp')->each(function ($item, $key){
-            if(!is_string($item) || empty($item)){
-                throw new SignException( $key . ' must be filled in');
+    protected function validateParams(array $request): array
+    {
+        $data = collect($request->header())->only('_sign', '_sign_type', '_timestamp');
+        if ($data->isEmpty()) {
+            throw new SignException('_sign and _sign_type and _timestamp must be filled in');
+        }
+
+        return $data->each(function ($item, $key) {
+            if (!is_string($item) || empty($item)) {
+                throw new SignException($key.' must be filled in');
             }
-            if($key === '_timestamp' && !$this->validateTimestamp($item)){
+            if ('_timestamp' === $key && !$this->validateTimestamp($item)) {
                 throw new SignException('request time out');
             }
         })->all();
     }
+
     /**
-     * 验证时间
+     * 验证时间.
+     *
      * @method validateTimestamp
+     *
      * @param $timestamp
      *
      * @return bool
      *
      * @author luffyzhao@vip.126.com
      */
-    protected function validateTimestamp($timestamp){
-       return !empty($timestamp) && Carbon::parse($timestamp)->diffInRealSeconds() > 60;
+    protected function validateTimestamp($timestamp)
+    {
+        return !empty($timestamp) && Carbon::parse($timestamp)->diffInRealSeconds() > 60;
     }
 
     /**
      * 获取加签对象
+     *
      * @method signObj
+     *
      * @param $signType
      *
      * @return Md5Sign|RsaSign
+     *
      * @throws SignException
      *
      * @author luffyzhao@vip.126.com
      */
-    protected function signObj($signType){
-        switch(strtoupper($signType)){
+    protected function signObj($signType)
+    {
+        switch (strtoupper($signType)) {
             case 'MD5':
                 $signObj = new Md5Sign();
                 break;
@@ -97,6 +125,7 @@ class SignManager
             default:
                 throw new SignException('sign type must be filled in');
         }
+
         return $signObj;
     }
 }
