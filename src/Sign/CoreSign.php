e1c3105524918f164d8d032f9483ddbab473dda3
luffyzhao <luffyzhao@vip.126.com>
2018-05-31 11:19:50 +0800

    兼容7.2

--- src/Sign/CoreSign.php
+++ src/Sign/CoreSign.php
@@ -17,7 +17,7 @@ abstract class CoreSign
      *
      * @author luffyzhao@vip.126.com
      */
-    protected function sortKeys(array $data): array
+    public function sortKeys(array $data): array
     {
         return (new Collection($data))->map(function ($item) {
             if (is_object($item) || is_array($item)) {
@@ -39,19 +39,21 @@ abstract class CoreSign
      *
      * @author luffyzhao@vip.126.com
      */
-    protected function createLinkstring(array $data, $pix = ''): string
+    public function createLinkstring(array $data, $pix = ''): string
     {
         $sign = '';
-        while (list($key, $val) = each($data)) {
-            $key = ('' === $pix ? $key : $pix."[{$key}]");
-            if (is_object($val) || is_array($val)) {
-                $sign .= $this->createLinkstring((array) $val, $key).'&';
-            } else {
-                $sign .= $key.'='.$val.'&';
+        if (!empty($data)) {
+            foreach ($data as $key => $val) {
+                $key = ('' === $pix ? $key : $pix."[{$key}]");
+                if (is_object($val) || is_array($val)) {
+                    $sign .= $this->createLinkstring((array) $val, $key).'&';
+                } else {
+                    $sign .= $key.'='.$val.'&';
+                }
             }
         }
         //去掉最后一个&字符
-        $sign = substr($sign, 0, count($sign) - 2);
+        $sign = substr($sign, 0, mb_strlen($sign) - 1);
         //如果存在转义字符，那么去掉转义
         if (get_magic_quotes_gpc()) {
             $sign = stripslashes($sign);
