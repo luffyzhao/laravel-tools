f7e62351520a425fc0e2b80670977c0f1086e792
luffyzhao <luffyzhao@vip.126.com>
2022-08-26 15:07:33 +0800

    refresh

---
+++ src/Libraries/VueMessage.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Collection;
+use JsonSerializable;
+
+class VueMessage implements JsonSerializable
+{
+    protected $data;
+
+    public function __construct()
+    {
+        $this->data = Collection::make();
+    }
+
+    /**
+     * @param $code
+     * @param $message
+     * @return void
+     */
+    public function add($code, $message){
+        $this->data->add([
+            'code'=>$code, 'message'=>$message
+        ]);
+    }
+
+
+    /**
+     * @return array|mixed
+     */
+    public function jsonSerialize()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getData(): Collection
+    {
+        return $this->data;
+    }
+}
